291701c0650bb6929f3580374d2fffe01bbd0d1d
moehzi <faizi1234098@gmail.com>
2022-03-10 20:49:24 +0800

    feat: using redux ;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, mulitply } from './actions';
+import { useState } from 'react';
 
 function App() {
-  return <div>Hello</div>;
+  const counter = useSelector((state) => state.counter);
+  const dispacth = useDispatch();
+  const [multiplyNum, setMultiplyNum] = useState(0);
+  return (
+    <div className="App">
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispacth(increment())}>+</button>
+      <button onClick={() => dispacth(decrement())}>-</button>
+      <div>
+        <input
+          type="number"
+          value={multiplyNum}
+          style={{ padding: '5px' }}
+          onChange={(e) => setMultiplyNum(e.target.value)}
+        ></input>
+        <button onClick={() => dispacth(mulitply(multiplyNum))}>
+          Multiply
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/reducers/counter.js
@@ -0,0 +1,15 @@
+const counterReducer = (state = 0, action) => {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    case 'MULTIPLY':
+      return state * action.payload;
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
